f9f8baba89dd27b48061866ae448b39f2b233e1b
kusano <kusano@users.noreply.github.com>
2023-03-30 03:17:53 +0900

    焼きなまし。
    39764355974
    22,161,333,384

--- A.cpp
+++ A.cpp
@@ -5,7 +5,38 @@
 #include <algorithm>
 #include <chrono>
 #include <cstdio>
+#include <cmath>
 using namespace std;
+
+const double TIME = 5.8;
+
+const int expN = 1024;
+const double expX = 16;
+int expT[expN];
+
+void my_exp_init()
+{
+    for (int x=0; x<expN; x++)
+    {
+        double x2 = (double)-x/expN*expX;
+        double e = exp(x2)*0x80000000+.5;
+        if (e>=0x7fffffff)
+            expT[x] = 0x7fffffff;
+        else
+            expT[x] = int(e);
+    }
+}
+
+//  exp(t)*0x80000000;
+int my_exp(double x)
+{
+    int x2 = int(x/-expX*expN+.5);
+    if (x2<0)
+        return expT[0];
+    if (x2>=expN)
+        return expT[expN-1];
+    return expT[x2];
+}
 
 struct Point
 {
@@ -247,6 +278,23 @@ Score calcScore(int D, const vector<int> &A1, const vector<int> &A2)
     return s;
 }
 
+void output(int D, const vector<int> &A1, const vector<int> &A2)
+{
+    int n = 0;
+    for (int a: A1)
+        n = max(n, a);
+    for (int a: A2)
+        n = max(n, a);
+    cout<<n<<endl;
+
+    for (int i=0; i<D*D*D; i++)
+        cout<<(i==0?"":" ")<<A1[i];
+    cout<<endl;
+    for (int i=0; i<D*D*D; i++)
+        cout<<(i==0?"":" ")<<A2[i];
+    cout<<endl;
+}
+
 int main()
 {
     int D;
@@ -258,10 +306,7 @@ int main()
 
     chrono::system_clock::time_point start = chrono::system_clock::now();
 
-    vector<int> bestA1;
-    vector<int> bestA2;
-    Score bestScore;
-    bestScore.score = 999999999999999999;
+    my_exp_init();
 
     int volume = D*D*D;
     for (int i=0; i<2; i++)
@@ -284,62 +329,59 @@ int main()
         volume = min(volume, v);
     }
 
-    int iter;
-    for (iter=0; ; iter++)
+    vector<Point> P1, P2;
+    vector<int> RT1, RT2;
+
+    auto randomPos1 = [&]() -> Point
     {
-        chrono::system_clock::time_point now = chrono::system_clock::now();
-        double time = chrono::duration_cast<chrono::nanoseconds>(now-start).count()*1e-9/5.0;
-        if (time>=1.)
-            break;
-
-        int maxn = min(volume, 50);
-        int n = xor64()%(maxn-1)+1;
-
-        vector<Point> P1(n), P2(n);
-        vector<int> RT1(n), RT2(n);
-        for (int i=0; i<n; i++)
+        while (true)
         {
-            Point p1, p2;
-            while (true)
-            {
-                p1.x = xor64()%D;
-                p1.y = xor64()%D;
-                p1.z = xor64()%D;
+            Point p(xor64()%D, xor64()%D, xor64()%D);
+            if (F1[p.z*D+p.x]==0 ||
+                R1[p.z*D+p.y]==0)
+                continue;
 
-                if (F1[p1.z*D+p1.x]==0 ||
-                    R1[p1.z*D+p1.y]==0)
-                    continue;
-
-                bool ok = true;
-                for (Point p: P1)
-                    if (p1==p)
-                        ok = false;
-                if (!ok)
-                    continue;
-
-                p2.x = xor64()%D;
-                p2.y = xor64()%D;
-                p2.z = xor64()%D;
-
-                if (F2[p2.z*D+p2.x]==0 ||
-                    R2[p2.z*D+p2.y]==0)
-                    continue;
-
-                for (Point p: P2)
-                    if (p2==p)
-                        ok = false;
-                if (!ok)
-                    continue;
-
-                break;
-            }
-            P1[i] = p1;
-            P2[i] = p2;
-
-            RT1[i] = xor64()%24;
-            RT2[i] = xor64()%24;
+            bool ok = true;
+            for (int i=0; i<(int)P1.size() && ok; i++)
+                if (P1[i]==p)
+                    ok = false;
+            if (!ok)
+                continue;
+            return p;
         }
+    };
 
+    auto randomPos2 = [&]() -> Point
+    {
+        while (true)
+        {
+            Point p(xor64()%D, xor64()%D, xor64()%D);
+            if (F2[p.z*D+p.x]==0 ||
+                R2[p.z*D+p.y]==0)
+                continue;
+
+            bool ok = true;
+            for (int i=0; i<(int)P2.size() && ok; i++)
+                if (P2[i]==p)
+                    ok = false;
+            if (!ok)
+                continue;
+            return p;
+        }
+    };
+
+    P1.push_back(randomPos1());
+    P2.push_back(randomPos2());
+    RT1.push_back(xor64()%24);
+    RT2.push_back(xor64()%24);
+
+    vector<int> bestA1;
+    vector<int> bestA2;
+    Score bestScore;
+    bestScore.score = 999999999999999999;
+
+    auto evaluate = [&](const vector<Point> &P1, const vector<int> &RT1, const vector<Point> &P2, const vector<int> &RT2) -> double
+    {
         pair<vector<int>, vector<int>> A = expand(D, F1, R1, F2, R2, P1, RT1, P2, RT2);
         vector<int> A1 = A.first;
         vector<int> A2 = A.second;
@@ -351,21 +393,83 @@ int main()
             bestA2 = A2;
             bestScore = score;
         }
+        return score.score*1e-9;
+    };
+    double score = evaluate(P1, RT1, P2, RT2);
+
+    double temp_inv;
+    int iter;
+    for (iter=0; ; iter++)
+    {
+        if (iter%0x100==0)
+        {
+            chrono::system_clock::time_point now = chrono::system_clock::now();
+            double time = chrono::duration_cast<chrono::nanoseconds>(now-start).count()*1e-9/TIME;
+            if (time>1.0)
+                break;
+            double temp = (1.0-time);
+            temp_inv = 1./temp;
+        }
+
+        vector<Point> P1n = P1;
+        vector<int> RT1n = RT1;
+        vector<Point> P2n = P2;
+        vector<int> RT2n = RT2;
+
+        int target = xor64()%(int)P1n.size();
+        switch (xor64()%6)
+        {
+        case 0:
+            // ブロックの追加
+            if ((int)P1n.size()>=volume-1)
+                continue;
+            P1n.push_back(randomPos1());
+            RT1n.push_back(xor64()%24);
+            P2n.push_back(randomPos2());
+            RT2n.push_back(xor64()%24);
+            break;
+        case 1:
+            // ブロックの削除
+            if ((int)P1n.size()<=1)
+                continue;
+            P1n.erase(P1n.begin()+target);
+            RT1n.erase(RT1n.begin()+target);
+            P2n.erase(P2n.begin()+target);
+            RT2n.erase(RT2n.begin()+target);
+            break;
+        case 2:
+            // 1個目を移動
+            P1n[target] = randomPos1();
+            break;
+        case 3:
+            // 2個目を移動
+            P2n[target] = randomPos2();
+            break;
+        case 4:
+            // 1個目を回転
+            RT1n[target] = xor64()%24;
+            break;
+        case 5:
+            // 2個目を回転
+            RT2n[target] = xor64()%24;
+            break;
+        }
+
+        double scoren = evaluate(P1n, RT1n, P2n, RT2n);
+
+        if (scoren<score ||
+            //exp((score2-score)*temp_inv)*0x80000000>xor64())
+            my_exp((score-scoren)*temp_inv)>xor64())
+        {
+            score = scoren;
+            P1 = P1n;
+            RT1 = RT1n;
+            P2 = P2n;
+            RT2 = RT2n;
+        }
     }
 
-    int n = 0;
-    for (int a: bestA1)
-        n = max(n, a);
-    for (int a: bestA2)
-        n = max(n, a);
-    cout<<n<<endl;
-
-    for (int i=0; i<D*D*D; i++)
-        cout<<(i==0?"":" ")<<bestA1[i];
-    cout<<endl;
-    for (int i=0; i<D*D*D; i++)
-        cout<<(i==0?"":" ")<<bestA2[i];
-    cout<<endl;
+    output(D, bestA1, bestA2);
 
     chrono::system_clock::time_point now = chrono::system_clock::now();
     double time = chrono::duration_cast<chrono::nanoseconds>(now-start).count()*1e-9;
